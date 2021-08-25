da12b56cd4c45f96c12d17126138a5ee58d5d7ae
hiep <hiephkhuu@gmail.com>
2021-08-25 14:35:24 -0700

    add reward form

---
+++ app/forms/reward_form.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, NumberRange, Length
+
+class TaskForm(FlaskForm):
+    task_id = StringField('task_id', validators=[DataRequired()])
+    reward_name = StringField('reward_name', validators=[DataRequired('Reward needs a name'), Length(max=255, message='Name must be fewer than 255 characters!') ])
+    reward_detail = StringField('reward_detail', validators=[Length(max=255, message='Details must be fewer than 255 characters!') ])
+    reward_reason = StringField('reward_reason', validators=[Length(max=255, message='Reasons must be fewer than 255 characters!') ])
+    reward_points = IntegerField('reward_points', validators=[DataRequired('Enter how much points this reward is worth'), NumberRange(min=66, message='Must be greater than 66! It takes around 66days for a task to become a habit!')])
+    color_id = StringField('color_id')
+
+
