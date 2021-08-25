1a318779a742e8e4d464f191f9ebce00604101fb
hiep <hiephkhuu@gmail.com>
2021-08-25 11:40:46 -0700

    fixed logs

--- app/seeds/logs.py
+++ app/seeds/logs.py
@@ -2,16 +2,25 @@ from app.models import db, Log
 
 
 def seed_logs():
-    l1 = Log(task_id=1)
+    l1 = Log(task_id=1,created_at= '2021-08-25 12:00:00')
     l2 = Log(task_id=2, completed=True);
-    l3 = Log(task_id=1, completed=True, created_at= '2021-08-24 11:17:38')
+    l3 = Log(task_id=1, completed=True, created_at= '2021-08-24 12:00:00')
+    l4 = Log(task_id=1, completed=True, created_at= '2021-08-24 12:00:00')
+    l5 = Log(task_id=1, completed=True, created_at= '2021-08-24 12:00:00')
+    l6 = Log(task_id=1, completed=True, created_at= '2021-08-24 12:00:00')
+    l7 = Log(task_id=1, completed=True, created_at= '2021-08-24 12:00:00')
+    l8 = Log(task_id=1, completed=True, created_at= '2021-08-24 12:00:00')
+    l9 = Log(task_id=1, completed=True, created_at= '2021-08-24 12:00:00')
+    l10 = Log(task_id=1, completed=True, created_at= '2021-08-24 12:00:00')
 
 
-    logs= [
-        l1,
-        l2,
-        l3
-    ]
+    logs= []
+
+    i = 1
+    while(i < 11):
+        logs.append('l'+ str(i))
+        i+=1
+
     for l in logs:
         db.session.add(l)
     db.session.commit()
