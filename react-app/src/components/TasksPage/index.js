c4007d0b9d17938eae16961ef7c6ebefa65f9f6e
hiep <hiephkhuu@gmail.com>
2021-08-26 12:19:18 -0700

    rendered user's tasks

--- react-app/src/components/TasksPage/index.js
+++ react-app/src/components/TasksPage/index.js
@@ -4,13 +4,39 @@ import {loadAllTasks} from '../../store/tasks'
 
 const TasksPage = () => {
   const sessionUser = useSelector(state => state.session.user)
+  const allTasks = useSelector(state => state.tasks)
+
   const dispatch = useDispatch()
-  console.log('#############',sessionUser.id)
-  useEffect(() => {
-    dispatch(loadAllTasks(sessionUser.id))
+  // console.log('#############',sessionUser.id)
+  // console.log('#########', allTasks)
+  useEffect(async () => {
+   await dispatch(loadAllTasks(sessionUser.id))
   }, [])
+
+  const grabTask = () => {
+    let taskList = []
+    for (const task in allTasks){
+
+      taskList.push(
+        <div className='task-bar'>
+          <p> {allTasks[task].task_name}</p>
+          <p> {allTasks[task].task_detail}</p>
+          <p> {allTasks[task].task_reason}</p>
+          <p> {allTasks[task].target_num}</p>
+          <p> {allTasks[task].task_points}</p>
+          <p> {allTasks[task].created_at}</p>
+        </div>
+
+      )
+    }
+    return taskList
+  }
+
   return (
-    <h1>TaskPage</h1>
+    <div>
+      <h1>TaskPage</h1>
+      {grabTask()}
+    </div>
   )
 }
 
