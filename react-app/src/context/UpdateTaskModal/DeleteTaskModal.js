69293abd8771513e5dc2df1422413087bdb5103e
hiep <hiephkhuu@gmail.com>
2021-08-27 13:06:32 -0700

    added delete modal

---
+++ react-app/src/context/UpdateTaskModal/DeleteTaskModal.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { Modal } from '../Modal';
+import { useSelector, useDispatch } from 'react-redux';
+import { Redirect, useHistory, useParams} from 'react-router-dom';
+
+import { deleteSingleTask } from '../../store/tasks';
+
+const DeleteTaskModal = ({}) => {
+  
+}
