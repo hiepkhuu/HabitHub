41a3371115c69a82531316a095ab2f92ef725013
hiep <hiephkhuu@gmail.com>
2021-08-30 00:06:32 -0700

    added greeting page

--- react-app/src/components/GreetingPage/index.js
+++ react-app/src/components/GreetingPage/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useSelector } from 'react-redux'
 import { NavLink, Redirect } from 'react-router-dom'
 
@@ -6,6 +6,22 @@ import { NavLink, Redirect } from 'react-router-dom'
 const GreetingPage = () => {
 
   const sessionUser = useSelector(state => state.user)
+  let quote;
+  const fetchQuote = () =>{
+    fetch("https://zenquotes.io/api/today")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      quote = data
+      console.log(quote)
+    });
+  }
+
+  useEffect(() => {
+    fetchQuote()
+  }, [])
+
 
 
   return (
@@ -17,9 +33,9 @@ const GreetingPage = () => {
       <h1>greeting page</h1>
       <h1>greeting page</h1>
       <h1>greeting page</h1>
+      <h1>{quote}</h1>
       <h1>greeting page</h1>
-      <h1>greeting page</h1>
-
+      {fetchQuote()}
       <NavLink to={`/dashboard`}>
         <button>go to task log</button>
       </NavLink>
