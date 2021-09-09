546014ec57b2a51987cf3fdcfa33686e5742fef2
hiep <hiephkhuu@gmail.com>
2021-09-09 14:53:22 -0700

    create backend and store for true logs

--- react-app/src/store/logs.js
+++ react-app/src/store/logs.js
@@ -1,7 +1,13 @@
 const GET_LOGS = 'logs/GET_LOG';
+const GET_TRUE_LOGS = 'logs/GET_TRUE_LOGS'
 
 const getLogs= (log) => ({
   type: GET_LOGS,
+  payload: log
+})
+
+const getTrueLogs = (log) => ({
+  type: GET_TRUE_LOGS,
   payload: log
 })
 
@@ -11,11 +17,18 @@ const getLogs= (log) => ({
   dispatch(getLogs(data))
 }
 
+export const getTrueLogs = (id) => async (dispatchEvent) =>{
+  const res = await fetch(`/api/logs/completed/${id}`)
+  let data = await res.json();
+  dispatch(getTrueLogs(data))
+}
 let initialState = {}
 export default function reducer(state=initialState, action){
   switch (action.type) {
     case GET_LOGS:
-      return {...state,...action.payload}
+      return {all_logs: action.payload}
+    case GET_TRUE_LOGS:
+      return {true_logs: action.payload}
     default:
       return state
   }
