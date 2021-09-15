5915ce35cedc951f1c6fcab60bef8bb6f14c17fe
hiep <hiephkhuu@gmail.com>
2021-09-15 15:54:57 -0700

    add log store

--- react-app/src/store/logs.js
+++ react-app/src/store/logs.js
@@ -1,5 +1,6 @@
 const GET_LOGS = 'logs/GET_LOG';
 const GET_TRUE_LOGS = 'logs/GET_TRUE_LOGS'
+const ADD_LOG = 'logs/ADD_LOGS'
 
 const getLogs= (log) => ({
   type: GET_LOGS,
@@ -8,6 +9,11 @@ const getLogs= (log) => ({
 
 const getTrueLogs = (log) => ({
   type: GET_TRUE_LOGS,
+  payload: log
+})
+
+const addALog = (log) => ({
+  type: ADD_LOG,
   payload: log
 })
 
@@ -22,6 +28,35 @@ export const getAllCompletedLogs = (id) => async (dispatch) =>{
   let data = await res.json();
   dispatch(getTrueLogs(data))
 }
+
+//post log route
+export const addCompletedLog = (log) => async (dispatch) => {
+  const res = await fetch('/api/logs/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(
+      log
+    ),
+  })
+
+  if (res.ok){
+    const data = await res.json()
+    dispatch(addALog(data))
+    return null;
+  }else if (res.status < 500) {
+    const data = await res.json();
+
+    if (data.errors) {
+      return data.errors;
+    }
+  } else {
+    return ['An error occurred. Please try again.']
+  }
+}
+
+
 let initialState = {}
 export default function reducer(state=initialState, action){
   switch (action.type) {
@@ -29,6 +64,8 @@ export default function reducer(state=initialState, action){
       return {all_logs: action.payload}
     case GET_TRUE_LOGS:
       return {true_logs: action.payload}
+    case ADD_LOG:
+      return {added_log: action.payload}
     default:
       return state
   }
