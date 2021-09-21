9d3285eeca1ad3e6b890f6dba0bee0737e6f5e72
hiep <hiephkhuu@gmail.com>
2021-09-20 18:21:57 -0700

    changed color sheme

--- react-app/src/components/GreetingPage/index.js
+++ react-app/src/components/GreetingPage/index.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { NavLink, Redirect } from 'react-router-dom'
 import {getSingleQuote} from '../../store/quotes'
+import moment from 'moment'
 
 import './GreetingPage.css'
 
@@ -18,21 +19,35 @@ const GreetingPage = () => {
     await dispatch(getSingleQuote())
   }, [])
 
+  function convert(input) {
+    return moment(input, 'HH:mm:ss').format('h:mm A');
+    // .format('h:mm:ss A')
+  }
+
+  const todayDate = () => {
+    const date = moment().toDate().toString()
+    const splitDate = date.split(' ')
+    const newDate = splitDate.slice(0,1) + ', ' + splitDate.slice(1,2) + ' ' + splitDate.slice(2,3 ) +', ' + splitDate.slice(3,4)
+    // const newDate = splitDate.slice(0,1) + ', ' + splitDate.slice(1,2) + ' ' + splitDate.slice(2,3 ) +', ' + splitDate.slice(3,4) + ' ' + convert(splitDate.slice(4, 5))
+    return newDate
+
+  }
+
+
 
   return (
     <div className='greet-card'>
       <div className='greet-image'></div>
-      <h1>Hey {sessionUser.first_name}! Great to have you back.</h1>
-      <h3>Ready to do some gardening?</h3>
+      <h1>Hello {sessionUser.first_name}</h1>
+      <h1>Today is {todayDate()} </h1>
+      <h3>Ready to start some habits?</h3>
       <div className='greeting'>
 
       <div>
         <h2>"{quote?.quote}"</h2>
         <h2>- {quote?.author}</h2>
       </div>
-      {/* <NavLink to={`/dashboard`}>
-        <button>Habit Dashboard =></button>
-  </NavLink>*/}
+
       </div>
     </div>
   )
