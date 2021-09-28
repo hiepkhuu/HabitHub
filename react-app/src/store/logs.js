d4e7124ffeb464da91bbc2247555d853869ef3ed
hiep <hiephkhuu@gmail.com>
2021-09-28 14:44:26 -0700

    created thunk for weekly log" ;

--- react-app/src/store/logs.js
+++ react-app/src/store/logs.js
@@ -1,6 +1,12 @@
 const GET_LOGS = 'logs/GET_LOG';
 const GET_TRUE_LOGS = 'logs/GET_TRUE_LOGS'
 const ADD_LOG = 'logs/ADD_LOGS'
+const GET_WEEKLY_LOGS = 'logs/GET_WEEKLY_LOGS'
+
+const getWeeklyLogs = (log) => ({
+  type: GET_WEEKLY_LOGS,
+  payload: log
+})
 
 const getLogs= (log) => ({
   type: GET_LOGS,
@@ -17,12 +23,21 @@ const addALog = (log) => ({
   payload: log
 })
 
+//GET ALL COMPLETED/UNCOMPLETED LOGS FOR ONE TASK
  export const getAllLogs = (id) => async (dispatch) => {
   const res = await fetch(`/api/logs/${id}`)
   let data = await res.json();
   dispatch(getLogs(data))
 }
 
+//GET WEEKLY LOGS COMPLETED/UNCOMPLETED FOR ALL TASKS WITHIN THE WEEK
+export const getAllWeeklyLogs = () => async (dispatch) => {
+  const res = await fetch(`/api/logs/week`)
+  let data = await res.json();
+  dispatch(getWeeklyLogs(data))
+}
+
+//GET ONLY COMPLETED LOG BY CERTAIN USER
 export const getAllCompletedLogs = (id) => async (dispatch) =>{
   const res = await fetch(`/api/logs/completed/${id}`)
   let data = await res.json();
@@ -66,6 +81,8 @@ export default function reducer(state=initialState, action){
       return {true_logs: action.payload}
     case ADD_LOG:
       return {added_log: action.payload}
+    case GET_WEEKLY_LOGS:
+      return {weekly_logs: action.payload}
     default:
       return state
   }
