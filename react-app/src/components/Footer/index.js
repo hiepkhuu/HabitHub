5b8aeadab5bbff4e2a958569c040c8253a61c38f
hiep <hiephkhuu@gmail.com>
2021-09-29 12:13:48 -0700

    footer added

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+// import {a} from 'react-router-dom'
 import './Footer.css'
 
 
@@ -7,9 +7,21 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      <Link className='footer-link' to='/'>By: Hiep Khuu</Link>
-      <Link className='footer-link' to='https://www.linkedin.com/in/hiep-khuu-380111201/'>linkedin</Link>
-      <Link className='footer-link' to='https://github.com/hiepkhuu'>Github</Link>
+      <div className='footer-icon'>
+
+      </div>
+      <div className='feature-links'>
+
+      </div>
+      <div className='font-awesome'>
+
+      </div>
+      <div className='developer-info'>
+          <h2>Developed By:</h2>
+          <a className='footer-link' to='/'>Hiep Khuu</a>
+          <a className='footer-link' to='https://www.aedin.com/in/hiep-khuu-380111201/'> <span></span>LinkedIn</a>
+          <a className='footer-link' to='https://github.com/hiepkhuu'><span></span>Github</a>
+      </div>
     </footer>
   )
 }
