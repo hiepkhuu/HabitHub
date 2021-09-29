446846e48a1013887d37c7ba02924a17aec3a474
hiep <hiephkhuu@gmail.com>
2021-09-29 13:01:59 -0700

    footer added

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -11,19 +11,22 @@ const Footer = () => {
           <div className='footer-icon'></div>
           <div className='feature-links'>
               <Link className='footer-link' to='/login'>Login</Link>
-              <Link className='footer-link' to='/signup'>Signup</Link>
+              <Link className='footer-link' to='/sign-up'>Signup</Link>
               <Link></Link>
           </div>
       </div>
-      <div className='font-awesome'>
-
-      </div>
       <div className='developer-info'>
-          <h2>Developed By:</h2>
-          <a className='footer-link' to='/'>Hiep Khuu</a>
-          <a className='footer-link' to='https://www.aedin.com/in/hiep-khuu-380111201/'> <span></span>LinkedIn</a>
-          <a className='footer-link' to='https://github.com/hiepkhuu'><span></span>Github</a>
+          <h3>DEVELOPED BY:</h3>
+          <a className='developer-name'>Hiep Khuu</a>
+          <div>
+            <a className='footer-link' href='https://www.aedin.com/in/hiep-khuu-380111201/'> <i class="fab fa-linkedin-in"></i></a>
+            <a className='footer-link' href='https://github.com/hiepkhuu'><i class="fab fa-github"></i></a>
+          </div>
       </div>
+      <div className='font-awesome'>
+        Icons Mady By <a className='footer-link awesome' href='https://fontawesome.com/'>Font Awesome</a>
+      </div>
+
     </footer>
   )
 }
