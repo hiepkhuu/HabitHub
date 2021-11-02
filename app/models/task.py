0d2e76fbfb0cbcf7d3f9e050de49932c0cd3e173
hiep <hiephkhuu@gmail.com>
2021-11-01 21:54:08 -0700

    fixed enourmouse deleteing error

--- app/models/task.py
+++ app/models/task.py
@@ -19,7 +19,7 @@ class Task(db.Model):
     rewards = db.relationship('Reward', back_populates='task')
     color = db.relationship('Color', back_populates='tasks')
     owner = db.relationship('User', back_populates='tasks')
-    logs = db.relationship('Log', back_populates='task')
+    logs = db.relationship('Log', back_populates='task', cascade="all, delete, delete-orphan", lazy=True)
     # def get_all_Tasks(self):
     #     def helper(n):
     #         for a in self.rewards:
